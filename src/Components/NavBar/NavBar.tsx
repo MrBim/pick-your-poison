ba846b5f401e2a3ab3c1ab31a7663070e59f968c
MrBim <bimtech84@gmail.com>
2022-02-26 18:36:19 +0000

    nav links back to a new search

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import styles from './NavBar.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
 const NavBar = () => {
   return (
-    <div className={styles.main}>- Pick your Poison -</div>
-  )
+    <div className={styles.main}>
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        - Pick your Poison -
+      </Link>
+    </div>
+  );
 };
 
 export default NavBar;
