085950a5fc6f9ec81e80b83c76b0c9827055e427
MrBim <bimtech84@gmail.com>
2022-02-26 11:24:43 +0000

    adding react select

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,34 +1,38 @@
-import React, {
-  Dispatch,
-  SetStateAction,
-  useMemo,
-  useState,
-} from "react";
+import React, { Dispatch, SetStateAction, useMemo, useState } from "react";
+import Select from "react-select";
 import { useQuery } from "react-query";
 import { getIngredients } from "../../Scripts/network-functions";
 import styles from "./SearchBar.module.css";
 
+type selectObject = { label: string; value: string };
 type Props = {
   setSearchTerm: Dispatch<SetStateAction<string>>;
 };
 
 const SearchBar = ({ setSearchTerm }: Props) => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState({ label: "", value: "" });
 
-  const {data, error } = useQuery('ingredients', getIngredients)
+  const { data, error } = useQuery("ingredients", getIngredients);
 
   const ingredients = useMemo(() => {
     if (!data) return;
-    return data.drinks; // TODO sort here
-  }, [data])
+    return data.drinks
+      .map((it: { strIngredient1: string }) => ({
+        value: it.strIngredient1,
+        label: it.strIngredient1,
+      }))
+      .sort((a: selectObject, b: selectObject) =>
+        a.label > b.label ? 1 : b.label > a.label ? -1 : 0
+      );
+  }, [data]);
 
   const handleSearchClick = () => {
-    setSearchTerm(inputValue);
+    setSearchTerm(inputValue.value);
   };
-  if (error) return <h6>there has been an error, oops</h6>
+  if (error) return <h6>there has been an error, oops</h6>;
   return (
     <div className={styles.main}>
-      <select
+      {/* <select
         className={styles.searchInput}
         value={inputValue}
         onChange={(e) => setInputValue(e.currentTarget.value)}
@@ -37,7 +41,15 @@ const SearchBar = ({ setSearchTerm }: Props) => {
         {ingredients && ingredients.map((it: Ingredient) => (
           <option key={it.strIngredient1} value={it.strIngredient1}>{it.strIngredient1}</option>
         ))}
-      </select>
+      </select> */}
+      <div style={{ width: "80%" }}>
+        <Select
+          value={inputValue}
+          name="search"
+          options={ingredients}
+          onChange={(e) => setInputValue(e ?? { label: "", value: "" })}
+        />
+      </div>
       <button className={styles.searchButton} onClick={handleSearchClick}>
         Search
       </button>
